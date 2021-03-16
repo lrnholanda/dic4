0668d8ee79574852b1e7cd6040da59189e86c6ea
lrnholanda <lrnholanda@gmail.com>
2021-03-16 20:22:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#a list of dictionaries
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
+#more realistic example
+
+#make an empty list for storing aliens.
+
+aliens = []
+
+#make 30 green aliens
+
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+#show the first 5 aliens.
+for alien in aliens[:6]:
+    print(alien)
+print("...")
+
+#show how many aliens have been created.
+
+print(f"total number of aliens: {len(aliens)}")
